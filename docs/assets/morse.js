6e4d77dd5808c3ecb64ee899df59d4a15a4eed50
otokitoki <53075562+otokitoki@users.noreply.github.com>
2021-01-09 04:18:20 +0900

    Update morse.js

--- docs/assets/morse.js
+++ docs/assets/morse.js
@@ -67,14 +67,14 @@ function init(){
           .then(res => {
             data = res.converted; // `data.json()` の呼び出しで解釈された JSON データ
             console.log(data);
-            data.replace(" ","");
+            data.replace("　","");
             console.log(data);
-            const text_kata = text.replace(/[ぁ-ん]/g, function(s) {
+            const data_kata = data.replace(/[ぁ-ん]/g, function(s) {
             return String.fromCharCode(s.charCodeAt(0) + 0x60);
           });
-          const morse = Array.prototype.map.call(text_kata,(c)=>{
+          const morse = Array.prototype.map.call(data_kata,(c)=>{
             return morse_obj[c];
-          });
+          //});
           const morse_space = morse.join("　")
           console.log(morse_space);
           document.getElementById("result").style.display = "block";
